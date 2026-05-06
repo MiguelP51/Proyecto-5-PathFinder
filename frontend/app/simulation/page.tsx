ff5577899e5734a7b6c57548d88395bfc972687b
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2026-05-05 19:04:33 -0500

    Estructura front completa

---
+++ frontend/app/simulation/page.tsx
@@ -0,0 +1,12 @@
+import styles from "../../styles/Landpage.module.css"
+
+export default function Simulation() {
+    return (
+        <main>
+            <div className={styles.container}>
+                simulation
+            </div>
+            
+        </main>
+    );
+}
