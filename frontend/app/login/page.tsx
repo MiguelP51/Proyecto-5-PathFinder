aeeb773f70d191dede6cc953326b21bd8333a69a
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2026-05-05 19:04:33 -0500

    Estructura front completa

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/LoginRegister.module.css"
+
+export default function Login() {
+    return (
+        <main>
+            <div className={styles.container}>
+                <div className={styles.welcomebox}>
+                </div>
+            </div>
+            
+        </main>
+    );
+}
