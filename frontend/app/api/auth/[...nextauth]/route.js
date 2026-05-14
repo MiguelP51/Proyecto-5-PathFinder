ec1f57ffab6bee31304249e9fbf7930b8c6d09ad
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2026-05-14 11:33:52 -0500

    Frontend maneja login DONE

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,10 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+    pages: {
+    signIn: '/login', // Página de login
+    error: '/login', // Redirige a login si hay error
+  },
     providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -12,14 +16,14 @@ export const authOptions = {
     async signIn({ user }) {
       // Aquí mandas los datos al backend Java
       try {
-        await fetch(`${process.env.BACKEND_URL}/auth/google`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            email: user.email,
-            nombre: user.name,
-          }),
-        });
+        // await fetch(`${process.env.BACKEND_URL}/auth/google`, {
+        //   method: "POST",
+        //   headers: { "Content-Type": "application/json" },
+        //   body: JSON.stringify({
+        //     email: user.email,
+        //     nombre: user.name,
+        //   }),
+        // });
         return true; // permite el login
       } catch {
         return false; // bloquea el login si el backend falla
