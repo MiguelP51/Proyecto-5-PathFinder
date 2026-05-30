640333503a515a3d952a782d9739b2785a617da8
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2026-05-30 17:50:02 -0500

    Add localhost to CORS

--- backend/src/main/java/com/pathfinder/config/SecurityConfig.java
+++ backend/src/main/java/com/pathfinder/config/SecurityConfig.java
@@ -99,6 +99,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "https://pathfinder.work.gd",
+                "http://localhost",
                 "http://localhost:3000"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
